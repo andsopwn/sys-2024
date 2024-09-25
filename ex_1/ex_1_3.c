37c32a51fee25edbf6c9ae2c6dfbd9270fb2b325
seo jun Lee <pwned@korea.edu>
2024-09-25 17:44:13 +0900

    4w assignment
    20240925

--- ex_1/ex_1_3.c
+++ ex_1/ex_1_3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 extern int addnum(int a, int b);
+extern int subnum(int a, int b);
 
 int main() {
     int sum;
 
     sum = addnum(1, 5);
     printf("Sum 1~5 = %d\n", sum);
+
+    printf("subnum - %d\n", subnum(10, 5));
 }
