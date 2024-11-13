604c4ff406e8a125d97a569b54eb54ae1d8db7dd
seo jun Lee <pwned@korea.edu>
2024-11-13 15:43:15 +0900

    11w assignment

---
+++ ex_5/ex_5_20.c
@@ -0,0 +1,12 @@
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+    struct tm *tm;
+    time_t timep;
+    time(&timep);
+    tm = localtime(&timep);
+    
+    printf("Time(sec) : %d\n", (int)timep);
+    printf("Time(date) : %s", asctime(tm));
+}
